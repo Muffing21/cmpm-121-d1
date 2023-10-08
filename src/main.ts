5675fafe13e991e9a1b8490745ea1530a6c5a37a
wichapas <wpichetp@ucsc.edu>
2023-10-08 16:22:46 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,23 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Plant the Trees";
+const gameName = "Wake Me Up";
+//const testing = "test";
+const buttonText = "💤🛌🏻😴";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+
 app.append(header);
+
+//create a button element
+const button: HTMLButtonElement = document.createElement("button");
+
+//set the button text
+button.type = "button";
+button.innerHTML = buttonText;
+app.append(button);
+
+//const mainButton: HTMLButtonElement = document.createElement("button");
