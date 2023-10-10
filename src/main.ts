caecfc156149085afce412ed9f1dc02228d3a4e3
wichapas <wpichetp@ucsc.edu>
2023-10-09 22:20:10 -0700

    Step 6 complete

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ const gameName = "Wake Me Up";
 let counter: number = 0;
 let timer: DOMHighResTimeStamp = 0;
 let then = performance.now();
-let itemBought: boolean = false;
+const item = { A: 0, B: 0, C: 0 };
+const itemCost = { A: 10, B: 100, C: 1000 };
+let growthRate: number = 0;
 
 document.title = gameName;
 
@@ -17,32 +19,64 @@ app.append(header);
 
 //create button elements
 const button: HTMLButtonElement = document.createElement("button");
-const upgradeButton1: HTMLButtonElement = document.createElement("button");
+const upgradeButtonA: HTMLButtonElement = document.createElement("button");
+const upgradeButtonB: HTMLButtonElement = document.createElement("button");
+const upgradeButtonC: HTMLButtonElement = document.createElement("button");
 
 //set the button text
 button.type = "button";
 button.textContent = "💤🛌🏻😴";
 //upgrade button text
-upgradeButton1.type = "button";
-upgradeButton1.textContent = "Cost 10: A Helping Hand";
+upgradeButtonA.type = "button";
+upgradeButtonA.textContent = "Cost 10: A Helping Hand";
+upgradeButtonB.type = "button";
+upgradeButtonB.textContent = "Cost 100: Slap Machine";
+upgradeButtonC.type = "button";
+upgradeButtonC.textContent = "Cost 1000: World Champion Slapper";
 
-//create slap object
+//create div object
 const slapDisplay: HTMLDivElement = document.createElement("div");
-slapDisplay.textContent = `slap count: ${counter}`;
+const growthDisplay: HTMLDivElement = document.createElement("div");
+const itemDisplayA: HTMLDivElement = document.createElement("div");
+const itemDisplayB: HTMLDivElement = document.createElement("div");
+const itemDisplayC: HTMLDivElement = document.createElement("div");
+
+slapDisplay.textContent = `slap count: ${counter.toFixed(1)}`;
+growthDisplay.textContent = `Growth Rate: ${growthRate} slaps/sec`;
+itemDisplayA.textContent = `number of Helping Hands: ${item.A}`;
+itemDisplayB.textContent = `number of Slap Machines: ${item.B}`;
+itemDisplayC.textContent = `number of World Slapping Champions: ${item.C}`;
 
 //append buttons
 app.append(button);
-app.append(upgradeButton1);
+app.append(upgradeButtonA);
+app.append(upgradeButtonB);
+app.append(upgradeButtonC);
+
 app.append(slapDisplay);
+app.append(growthDisplay);
+app.append(itemDisplayA);
+app.append(itemDisplayB);
+app.append(itemDisplayC);
+
 // setInterval(countFunction, 1000);
 
 //misc
 if (counter < 10) {
-  upgradeButton1.disabled = true;
+  upgradeButtonA.disabled = true;
+}
+if (counter < 100) {
+  upgradeButtonB.disabled = true;
+}
+if (counter < 1000) {
+  upgradeButtonC.disabled = true;
 }
 
 //event listener
-upgradeButton1.addEventListener("click", upgradeFunction);
+upgradeButtonA.addEventListener("click", upgradeFunction);
+upgradeButtonB.addEventListener("click", upgradeFunctionB);
+upgradeButtonC.addEventListener("click", upgradeFunctionC);
+
 button.addEventListener("click", countFunction);
 
 window.requestAnimationFrame(frameFunction);
@@ -54,22 +88,55 @@ function countFunction() {
 
 function frameFunction() {
   timer += performance.now() - then;
-  if (timer >= 1000 && itemBought == true) {
-    counter += 1;
-    slapDisplay.textContent = `slap count: ${counter}`;
-    console.log(counter);
+  if (timer >= 1000) {
+    if (item.A > 0) {
+      counter += 0.1;
+      slapDisplay.textContent = `slap count: ${counter.toFixed(1)}`;
+      itemDisplayA.textContent = `number of Helping Hands: ${item.A}`;
+      growthDisplay.textContent = `Growth Rate: ${growthRate} slaps/sec`;
+    }
+    if (item.B > 0) {
+      counter += 2;
+      slapDisplay.textContent = `slap count: ${counter.toFixed(1)}`;
+      itemDisplayB.textContent = `number of Slap Machines: ${item.B}`;
+      growthDisplay.textContent = `Growth Rate: ${growthRate} slaps/sec`;
+    }
+    if (item.C > 0) {
+      counter += 50;
+      slapDisplay.textContent = `slap count: ${counter.toFixed(1)}`;
+      itemDisplayC.textContent = `number of World Slapping Champions: ${item.C}`;
+      growthDisplay.textContent = `Growth Rate: ${growthRate} slaps/sec`;
+    }
     timer = 0;
   }
-  upgradeButton1.disabled = counter < 10;
+
+  upgradeButtonA.disabled = counter < itemCost.A;
+  upgradeButtonB.disabled = counter < itemCost.B;
+  upgradeButtonC.disabled = counter < itemCost.C;
 
   then = performance.now();
   window.requestAnimationFrame(frameFunction);
 }
 
 function upgradeFunction() {
-  if (counter >= 10) {
-    counter -= 10;
-    slapDisplay.textContent = `slap count: ${counter}`;
-    itemBought = true;
+  if (counter >= itemCost.A) {
+    counter -= itemCost.A;
+    item.A += 1;
+    growthRate += 0.1;
+  }
+}
+function upgradeFunctionB() {
+  if (counter >= itemCost.B) {
+    counter -= itemCost.B;
+    item.B += 1;
+    growthRate += 2;
+  }
+}
+
+function upgradeFunctionC() {
+  if (counter >= itemCost.C) {
+    counter -= itemCost.C;
+    item.C += 1;
+    growthRate += 50;
   }
 }
